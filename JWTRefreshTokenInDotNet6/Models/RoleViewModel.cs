c2b2a89f6e8a8f46a7245888903bee5ecdc4f672
خيال انسان <Hashem774346564@gmail.com>
2024-10-14 01:03:11 +0300

    Add project files.

---
+++ JWTRefreshTokenInDotNet6/Models/RoleViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace JWTRefreshTokenInDotNet6.Models
+{
+    public class RoleViewModel
+    {
+        public string? Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public bool Selected { get; set; }
+    }
+}
